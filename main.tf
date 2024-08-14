abdc57b8519713be18d1c705577a1e3db386ae2a
𝓙𝓪𝓰𝓭𝓪𝓶𝓫𝓪 𝓢𝓱𝓪𝓻𝓶𝓪 <154789084+shaheel03@users.noreply.github.com>
2024-08-15 00:25:02 +0530

    added rg

--- main.tf
+++ main.tf
@@ -1,10 +1,13 @@
-
+resource "azurerm_resource_group" "rg" {
+  name="rg1"
+  location = "west us"
+}
 resource "azurerm_resource_group" "rg2" {
-  name="rg3"
+  name="rg2"
   location = "west us"
 }
 
-resource "azurerm_resource_group" "rg1" {
-  name="rg2"
+resource "azurerm_resource_group" "rg3" {
+  name="rg3"
   location = "west us"
 }
